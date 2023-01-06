3ecfc9624d7c1f6bb02d52693c763240d87aed6e
Williams Balogun <williamsdev.official@gmail.com>
2023-01-06 18:34:28 +0100

    feat: modified the reducer and context functionality

--- src/context.jsx
+++ src/context.jsx
@@ -1,15 +1,24 @@
-import React, { useContext, useReducer, useState } from 'react'
+import React, { useContext, useReducer } from 'react'
 import { reducer } from './reducer'
 
 const contextAPI = React.createContext()
 const defaultState = {
   loading: false,
+  searchTerm: 'a',
+  drinks: [],
+  meals: [],
 }
 const GenContext = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, defaultState)
 
+  const handleSearch = (val) => {
+    dispatch({ type: 'SEARCH_TERM', payload: val })
+  }
+
   return (
-    <contextAPI.Provider value={{ ...state }}>{children}</contextAPI.Provider>
+    <contextAPI.Provider value={{ ...state, handleSearch }}>
+      {children}
+    </contextAPI.Provider>
   )
 }
 
--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,1 +1,11 @@
-export const reducer = (state, action) => {}
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SEARCH_TERM':
+      state = { ...state, searchTerm: action.payload }
+      break
+
+    default:
+      break
+  }
+  return state
+}
